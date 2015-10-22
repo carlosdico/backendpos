d7eea4881f0928416211942dd5453f6116cc26b6
Carlos Díez Cobo <carlosdico@yahoo.es>
2015-10-22 19:02:01 +0200

    Add TicketLines and Products Models

---
+++ app/Products.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Products extends Model
+{
+   
+	public $timestamps = false;
+    //
+    /**
+     * The database table used by the model.
+     *
+     * @var string
+     */
+    protected $table = 'PRODUCTS';
+
+    /**      * The attributes that are mass assignable.      *      * @var
+array      */     protected $fillable = ['ID', 'REFERENCE', 'CODE',
+'CODETYPE', 'NAME', 'PRICEBUY', 'PRICESELL', 'PRICESELL1', 'PRICESELL2', 'CATEGORY', 'TAXCAT', 'ATTRIBUTESET_ID', 'STOCKCOST', 'STOCKVOLUME', 'IMAGE', 'ISCOM', 'ISSCALE', 'ISKITCHEN', 'PRINTKB', 'SENDSTATUS', 'ISSERVICE', 'ATTRIBUTES', 'DISPLAY', 'ISVPRICE', 'ISVERPATRIB', 'TEXTTIP', 'WARRANTY', 'STOCKUNITS'];
+
+	/*public function ticket()
+    {
+        return $this->hasOne('App\Tickets');
+    }*/
+
+}
